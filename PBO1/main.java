d6941d1a3d9489dfce7ca013306d0698e214794f
Azhar Nur Hakim <azhar.nurhakim1@gmail.com>
2024-03-11 16:49:11 +0800

    POSTTEST1 PBO

---
+++ PBO1/main.java
@@ -0,0 +1,10 @@
+package PBO1;
+
+import javax.swing.JOptionPane;
+
+public class main {
+    public static void main(String[] args) throws Exception{
+        JOptionPane.showMessageDialog(null, "Hellow WOrld !!");
+    }
+    
+}
